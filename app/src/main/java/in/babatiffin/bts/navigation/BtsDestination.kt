57de0671106e430219f2ef6c97bcd8cdca218c9c
Rudra Sinha <84296638+rudrasinha05@users.noreply.github.com>
2026-09-07 04:16:41 +0530

    navigation: freeze core BTS routes

---
+++ app/src/main/java/in/babatiffin/bts/navigation/BtsDestination.kt
@@ -0,0 +1,14 @@
+package in.babatiffin.bts.navigation
+
+sealed class BtsDestination(val route: String) {
+    data object Home : BtsDestination("home")
+    data object Menu : BtsDestination("menu")
+    data object Plans : BtsDestination("plans")
+    data object Dashboard : BtsDestination("dashboard")
+    data object BuildMeal : BtsDestination("build-meal")
+    data object Subscription : BtsDestination("subscription")
+    data object Orders : BtsDestination("orders")
+    data object Nutrition : BtsDestination("nutrition")
+    data object Profile : BtsDestination("profile")
+    data object Support : BtsDestination("support")
+}
