74532e2bec678ceaaea32dcff201f8b9c4a2f160
Rudra Sinha <84296638+rudrasinha05@users.noreply.github.com>
2026-09-07 05:01:41 +0530

    fix: update home package namespace

--- app/src/main/java/in/babatiffin/bts/feature/home/HomeScreen.kt
+++ app/src/main/java/in/babatiffin/bts/feature/home/HomeScreen.kt
@@ -1,4 +1,4 @@
-package in.babatiffin.bts.feature.home
+package com.babatiffin.bts.feature.home
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
